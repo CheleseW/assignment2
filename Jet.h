578a545c50b2bbcfa554f6434b222b90003ef6fb
Chelese Washington <leecy98@gmail.com>
2019-10-04 10:11:07 -0400

    Jet.h has been created

---
+++ Jet.h
@@ -0,0 +1,27 @@
+//
+// Created by Esteban Parra on 9/5/19.
+//
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "Vehicle.h"
+
+class Jet : public Vehicle {
+
+private:
+    int numberOfEngines;
+
+public:
+    explicit Jet(string brand, string model, int numberOfEngines = 1);
+
+    virtual ~Jet();
+    int getEngineCount();
+    void setEngineCount(int gearCount);
+    virtual double mileageEstimate(double time);
+
+    virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_JET_H
